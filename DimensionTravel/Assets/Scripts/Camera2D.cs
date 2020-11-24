e5b423006aad7e9af0210467e6d0eeb0b0368b35
Hendrasworoe <hendrasworoe@gmail.com>
2020-11-24 10:56:19 +0700

    Adding code
    Reference code from https://github.com/jongallant/CarSimulator

---
+++ DimensionTravel/Assets/Scripts/Camera2D.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2D : MonoBehaviour
+{
+    public Transform area;
+
+    Vector3 currentPosition;
+    Vector3 currentScale;
+
+    public void Set()
+    {
+        float height = area.localScale.y * 100;
+        float width = area.localScale.x * 100;
+
+        float h = Screen.height / height;
+        float w = Screen.width / width;
+
+        float ratio = w / h;
+        float size = (height / 2) / 100f;
+
+        if (w<h)
+        {
+            size /= ratio;
+        }
+
+        Camera.main.orthographicSize = size;
+
+        Vector2 position = area.transform.position;
+
+        Vector3 camPosition = position;
+        Vector3 point = Camera.main.WorldToViewportPoint(camPosition);
+        Vector3 delta = camPosition - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+        Vector3 destination = transform.position + delta;
+
+        transform.position = destination;
+    }
+
+    public void LateUpdate()
+    {
+        if (currentPosition != area.transform.position || currentScale != area.transform.localScale)
+        {
+            currentPosition = area.transform.position;
+            currentScale = area.transform.localScale;
+            Set();
+        }
+    }
+}
