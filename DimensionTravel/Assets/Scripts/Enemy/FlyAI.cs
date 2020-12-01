04beafa6de653f8080de10dc847769222af1257e
Hendrasworoe <hendrasworoe@gmail.com>
2020-12-01 21:14:11 +0700

    Merge with other

---
+++ DimensionTravel/Assets/Scripts/Enemy/FlyAI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyAI : MonoBehaviour
+{
+    private Transform _player;
+    public float MoveSpeed = 5f;
+    public float stoppingDistance=10f;
+    public float retreatDistance=20f;
+    private float shotInterval;
+    private float startShot = 2;
+
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        shotInterval = startShot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tracking();
+        shot();
+    }
+
+    void tracking()
+    {
+        MoveSpeed = 5f;
+        if(Vector2.Distance(transform.position, _player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _player.position, MoveSpeed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, _player.position) < stoppingDistance && Vector2.Distance(transform.position, _player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }else if(Vector2.Distance(transform.position, _player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _player.position, - MoveSpeed * Time.deltaTime);
+        }
+            
+    }
+    
+    void shot()
+    {
+        if(shotInterval<=0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            shotInterval = startShot;
+        }
+        else
+        {
+            shotInterval -= Time.deltaTime;
+        }
+    }
+
+}
---
+++ DimensionTravel/Assets/Scripts/Enemy/Golem_alphaAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem_alphaAI : MonoBehaviour
+{
+    private float shotInterval;
+    private float startShot = 2;
+
+    public GameObject projectileGolem;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onewayprojectile();
+    }
+
+    void onewayprojectile()
+    {
+        if (shotInterval <= 0)
+        {
+            Instantiate(projectileGolem, transform.position, Quaternion.identity);
+            shotInterval = startShot;
+        }
+        else
+        {
+            shotInterval -= Time.deltaTime;
+        }
+    }
+}
